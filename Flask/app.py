7f6e10f98047b4ecd3cf9af7aca001db4ccf1112
pinedm3 <pinedm3@unlv.nevada.edu>
2024-10-03 09:42:32 -0700

    testing form url redirection

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
+
+
+@app.route('/form/')
+def form_page():
+    return render_template("form.html")
+
+
+@app.route('/foobar')
+def foobar():
+    return 'Hi there, foobar!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
