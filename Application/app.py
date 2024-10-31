cd697ca7301096815a8ec9a731c1c6cb03ded65c
pinedm3 <pinedm3@unlv.nevada.edu>
2024-10-31 12:37:14 -0700

    Added Placeholder message

--- Application/app.py
+++ Application/app.py
@@ -1,5 +1,6 @@
 import gradio as gr
 from gradio import ChatMessage
+from gradio import Chatbot
 import random as rd
 from retriever import do_search
 from prototype import gemini_generate_search_terms as gen
@@ -11,6 +12,7 @@ class Interface:
         self.isTest = isTest
         if self.isTest:
             self.demo = gr.ChatInterface(self.outputTest, type="messages")
+            self.demo.chatbot.placeholder = "<strong>Hello, welcome to the AI-Powered Research Assistant!</strong><br>To begin, describe your research question and I'll fetch scholarly articles that match that topic."
         else:
             self.demo = gr.ChatInterface(self.output, type="messages")
 
