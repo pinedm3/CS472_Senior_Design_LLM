09464303dcb6c57cabf5947a4cb798bf51514907
thebuddyadrian <cosmicpegasus18@gmail.com>
2024-11-04 13:57:29 -0800

    renamed old kaggle api dataset

---
+++ database/kaggle_dataset.py
@@ -0,0 +1,15 @@
+import kagglehub
+import pandas as pd
+from beam import Image, function
+
+
+# Retrieves the articles from a Kaggle database as a list of dictionaries
+# Each article has a "title", "abstract, "doi", etc.
+# See https://www.kaggle.com/datasets/victornuez/latest-research-articles?select=phys_and_computsci_articles.csv for more info
+@function(cpu="1", image=Image().add_python_packages(["haystack-ai", "sentence-transformers"]))
+def get_articles(max_articles: int = -1) -> list[dict]:
+	path = kagglehub.dataset_download("victornuez/latest-research-articles")
+	df = pd.read_csv(path + "/phys_and_computsci_articles.csv")
+	if max_articles > 0:
+		df = df.iloc[:max_articles]
+	return df.to_dict(orient='records')
