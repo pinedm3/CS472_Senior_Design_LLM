041fd96de985fcfb4ab5c67f8cedf1db0bda6ad4
BlueOSmile <alanjrd41@gmail.com>
2024-11-16 16:45:43 -0800

    asyncio switched to createTasks instead of gather

--- retriever/retriever.py
+++ retriever/retriever.py
@@ -15,6 +15,7 @@ from database.pubmed_api import get_pubmed_articles
 from llm.gemini_api import generate_search_terms
 import asyncio
 import time 
+#Chooses the exact database and runs appropiate function
 async def dataBaseSelection(term : str, database: str, resultsPerSearch: int):
     articles = None
     docData = []
@@ -31,53 +32,35 @@ async def dataBaseSelection(term : str, database: str, resultsPerSearch: int):
         docData.append(Document(content=article["abstract"], meta={"title": article["title"],"link": article["link"]}))
     return docData
 
-async def articleSearchTasks(search_terms,database,results_per_search) -> list:
-    coros = []
+#calls dataBaseSelection as asyncio courotines
+async def articleSearchTasks(search_terms,database,results_per_search):
+    taskResults = []
     print("Getting articles...")
-    for term in search_terms:
-        coros.append(dataBaseSelection(term,database,results_per_search))
-        print(term)
-    asycRecs = asyncio.gather(*coros)
-    await asycRecs
-    return asycRecs
+    async with asyncio.TaskGroup() as tg:
+        for term in search_terms:
+            taskResults.append(tg.create_task(dataBaseSelection(term,database,results_per_search)))
+            print(term)
+    return taskResults
 
-def do_embedding_based_search(query: str, num_search_terms: int = 5, results_per_search: int = 25, database: str = "arxiv") -> list:
+async def do_embedding_based_search(query: str, num_search_terms: int = 5, results_per_search: int = 25, database: str = "arxiv") -> list:
     
     # Generate search terms
     print("Generating search terms...")
-        #Not sure if await matter
     t0 = time.time()
-    search_terms: list[str] = generate_search_terms(query, num_search_terms)
+    search_terms:  list[str] = generate_search_terms(query, num_search_terms)
     t1 = time.time()
     print("Took: %f. \nSearch terms generated:" % (t1-t0))
-    #ASYNCIO STuFF
+    
     docList = []
-
+    #ASYNCIO SEARCH ARTICLES
     
-    #await asycRecs
     t0 = time.time()
-    asycRecs = asyncio.run(articleSearchTasks(search_terms,database,results_per_search))
-    for indivList in asycRecs._result:
-        docList+= indivList        
-    t1 = time.time()
-    print("asycResults took: ", t1-t0)
-    
+    asycRecs = await articleSearchTasks(search_terms,database,results_per_search)
+    #APPEND seperated lists to one list
+   
     # Store documents
-    
     document_store = InMemoryDocumentStore(embedding_similarity_function="cosine")
-    # The articles will be retrieved in dictionary format (see database.py)
-    # The embeddings will only be generated from Title and Abstract, the rest of the fields will be metadata    for term in search_terms:
-    
-    # for term in search_terms:
-    #     if database == "arxiv":
-    #         for article in get_arxiv_articles(term, results_per_search):
-    #             docList.append(Document(content=article["abstract"], meta={"title": article["title"],"link": article["link"]}))
-    #     elif database == "pubmed":
-    #         for article in get_pubmed_articles(term, results_per_search):
-    #             docList.append(Document(content=article["abstract"], meta={"title": article["title"],"link": article["link"]}))
-    #     else:
-    #         raise Exception("Invalid database %s" % database)
-    
+
     model = "BAAI/bge-small-en-v1.5"
 
     # Use GPU if available
@@ -97,12 +80,15 @@ def do_embedding_based_search(query: str, num_search_terms: int = 5, results_per
     query_pipeline.add_component("text_embedder", text_embedder)
     query_pipeline.add_component("retriever", InMemoryEmbeddingRetriever(document_store=document_store, scale_score=True, top_k=num_search_terms*results_per_search))
     query_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
-
     
-    #before actually needed
+    
+    for indivList in asycRecs:
+        docList+= indivList._result        
+    t1 = time.time()
+    print("took: ", t1-t0)
     print("Indexing articles...")
     indexing_pipeline.run({"documents": docList})
-    print("Searching for articles...")
+    
     result = query_pipeline.run({"text_embedder":{"text": query}})
 
     return result['retriever']['documents']
