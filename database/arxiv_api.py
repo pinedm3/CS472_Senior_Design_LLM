11d3c830d446b486b356a07f78dcf06ca047264e
thebuddyadrian <cosmicpegasus18@gmail.com>
2024-11-25 15:53:46 -0800

    arxiv search now retries 3 times before erroring

--- database/arxiv_api.py
+++ database/arxiv_api.py
@@ -8,12 +8,19 @@ def get_arxiv_articles(query: str, max_results: int) -> list[dict]:
 	global client
 	if client is None:
 		client = arxiv.Client(page_size=100)
+	for i in range(3):
+		try:
+			search = arxiv.Search(
+				query = query,
+				max_results = max_results,
+				sort_by = arxiv.SortCriterion.Relevance
+			)
+			break
+		except Exception as e:
+			print("Arxiv error occured, retrying: %s" % e)
+			if i == 2:
+				raise
 
-	search = arxiv.Search(
-		query = query,
-		max_results = max_results,
-		sort_by = arxiv.SortCriterion.Relevance
-	)
 	results = client.results(search)
 	list_results = []
 	for result in list(results):
